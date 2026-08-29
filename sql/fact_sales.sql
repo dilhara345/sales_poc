04c0708af21f8ee3699b6ed8410e261f652af199
Sanduni Dilhara <your-email@example.com>
2026-08-29 21:02:35 +0530

    sales_pos project ci?cd pipeline build

---
+++ sql/fact_sales.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE sales_poc.fact_sales AS 
+SELECT r.order_id, 
+c.customer_id, 
+p.product_id, 
+r.order_date, 
+r.quantity, 
+r.unit_price, 
+ROUND(r.quantity * r.unit_price, 2) AS total_amount 
+FROM sales_poc.rawdata r 
+JOIN sales_poc.dim_customer c 
+ON r.customer_name = c.customer_name AND r.email = c.email 
+JOIN sales_poc.dim_product p 
+ON r.product = p.product AND r.category = p.category;
