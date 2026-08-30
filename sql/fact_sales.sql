4b7e184ee97b4fbfda74cfbc6013abd43c7f5a6a
Sanduni Dilhara <your-email@example.com>
2026-08-30 21:32:26 +0530

    add order table

--- sql/fact_sales.sql
+++ sql/fact_sales.sql
@@ -6,8 +6,8 @@ r.order_date,
 r.quantity, 
 r.unit_price, 
 ROUND(r.quantity * r.unit_price, 2) AS total_amount 
-FROM sales_poc.rawdata r 
+FROM sales_poc.dim_order r 
 JOIN sales_poc.dim_customer c 
-ON r.customer_name = c.customer_name AND r.email = c.email 
+ON r.customer_name = c.customer_name --AND r.email = c.email 
 JOIN sales_poc.dim_product p 
-ON r.product = p.product AND r.category = p.category;
+ON r.product = p.product --AND r.category = p.category;
